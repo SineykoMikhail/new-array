999c05019a8316ed62c5c75ffdee84a2447a30d6
Sineyko Mikhail <sineykomikhail@gmail.com>
2022-02-16 08:13:35 +0200

    Add initial project

---
+++ assets/index.js
@@ -0,0 +1,26 @@
+// const array1 = ["1", "2", "3"];
+// const array2 = ["4", "5", "6"];
+// const array3 = array1.concat(array2);
+// console.log(array3);
+
+// const array1 = ["1", "2", "3"];
+// console.log("array1:", array1);
+// const reversed = array1.reverse();
+// console.log("array1:", array1);
+
+// const num = [1, 2, 3];
+// const num2 = num.push(4, 5, 6);
+// console.log(num)
+
+// const num = [1, 2, 3];
+// num.unshift(4, 5, 6);
+// console.log(num);
+
+// const array1 = ["js", "css", "jq"];
+// console.log("Элементы до:" + array1);
+// const shifted = array1.shift();
+// console.log("Элементы после:" + array1);
+// console.log("Удален этот элемент:" + shifted);
+
+const array1 = [1, 2, 3, 4, 5];
+console.log(array1.slice(1));
